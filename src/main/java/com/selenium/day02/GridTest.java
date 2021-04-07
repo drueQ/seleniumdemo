9b06872e4bdb7ccd75a326b086ff6b1fbc0bd85c
drueQ <1098762177@qq.com>
2021-04-07 22:37:53 +0800

    add java -verbose

--- src/main/java/com/selenium/day02/GridTest.java
+++ src/main/java/com/selenium/day02/GridTest.java
@@ -8,6 +8,7 @@ public class GridTest {
     public void test_gird(){
         System.out.println("66 88");
         System.out.println("66 77");
+        System.out.println("java -verbose");
     }
 
 }
